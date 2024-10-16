61a67e6dc1fcb210188729006c40ab74dc31d440
Andrew <andrew.moshko@gmail.com>
2024-10-16 14:37:55 +0300

    feat: updates for new version

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ async def root(response_file_name: str, file: UploadFile = File(...)):
         raise HTTPException(400, detail="Invalid document type")
     if not response_file_name.endswith(".xlsx"):
         response_file_name = response_file_name + ".xlsx"
-    new_excel_file = TeamworkExcelParser(file, response_file_name).get_valid_format()
+    new_excel_file = TeamworkExcelParser(file.file, response_file_name).get_valid_format()
     headers = {'Content-Disposition': f'attachment; filename="{response_file_name}"'}
     return StreamingResponse(iter([new_excel_file.getvalue()]), headers=headers)
--- xlsx_parser.py
+++ xlsx_parser.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from io import StringIO, BytesIO
 from itertools import groupby
 from pprint import pprint
@@ -16,46 +17,50 @@ class TeamworkExcelParser:
         self.output_data = []
         self.first_part_data = []
         self.second_part_data = []
+
+    def highlight_value(self, s):
+        is_value = s == 'До 15 числа'
+        return ['font-weight: bold' if v else '' for v in is_value]
         
 
     def get_valid_format(self):
-        # excel_file =ExcelFile(self.file.file)
-        df = pandas.read_excel(self.file.file)
+        # Читаем содержимое файла
+        contents = self.file.read()
+
+        # Используем BytesIO для создания потокового объекта из байтов
+        df = pandas.read_excel(BytesIO(contents))
+
+        # Продолжаем обработку данных
         self._group_tasks(df.to_dict(orient="records"))
         new_excel_data = pandas.DataFrame.from_records(self.output_data)
+
         output = BytesIO()
         writer = pandas.ExcelWriter(output, engine='xlsxwriter')
+
+        styler = new_excel_data.style
+        styler.apply(lambda x: ['font-weight: bold' if v == 'До 15 числа' and i == 'description' else '' for i, v in
+                                zip(x.index, x)], axis=1)
+
         new_excel_data.to_excel(writer, sheet_name='sheetName', index=False, na_rep='NaN', header=True)
+
         for column in new_excel_data:
             column_length = max(new_excel_data[column].astype(str).map(len).max(), len(column))
             col_idx = new_excel_data.columns.get_loc(column)
             writer.sheets['sheetName'].set_column(col_idx, col_idx, min(column_length, 50))
+
         writer.close()
+        output.seek(0)  # Сбрасываем указатель в начало файла
         return output
 
     @staticmethod
     def __key_func(k):
-        return k['Task Id']
-
-    def _get_date_key(self, data):
-        """
-            "Date" format = "08/03/2023"
-            "Date/Time" format = "08/01/2023 17:06"
-        :param data:
-        :return:
-        """
-
-        try:
-            return "Date" if "Date" in data[0] else "Date/Time"
-        except IndexError:
-            return "Date"
+        return k['Task ID']
 
     def _group_tasks(self, data):
         # sort INFO data by 'company' key.
         data = sorted(data, key=self.__key_func)
-        date_key = self._get_date_key(data)
-        first_part_data = [log for log in data if log[date_key].day <= 15]
-        second_part_data = [log for log in data if log[date_key].day > 15]
+        first_part_data = [log for log in data if datetime.strptime(log["Date"], "%m/%d/%Y").day <= 15]
+        second_part_data = [log for log in data if datetime.strptime(log["Date"], "%m/%d/%Y").day > 15]
 
         for key, value in groupby(data, self.__key_func):
             value = list(value)
@@ -75,7 +80,7 @@ class TeamworkExcelParser:
         self.first_part_data = sorted(self.first_part_data, key=lambda log: log["project"])
         self.second_part_data = sorted(self.second_part_data, key=lambda log: log["project"])
         
-        hours = sum([log["hours"] for log in self.output_data])
+        hours = round(sum([log["hours"] for log in self.output_data]), 2)
         self.output_data.append(dict(project="", task_link="", description="", 
                                      hours=hours, estimated_hours="", parent_task_with_task=""))
         self.output_data.append(dict(project="", task_link="", description="",
@@ -84,7 +89,7 @@ class TeamworkExcelParser:
                                      hours="", estimated_hours="", parent_task_with_task=""))
         
         self.output_data.extend(self.first_part_data)
-        hours = sum([log["hours"] for log in self.first_part_data])
+        hours = round(sum([log["hours"] for log in self.first_part_data]), 2)
         self.output_data.append(dict(project="", task_link="", description="",
                                      hours=hours, estimated_hours="", parent_task_with_task=""))
         self.output_data.append(dict(project="", task_link="", description="",
@@ -92,22 +97,22 @@ class TeamworkExcelParser:
         self.output_data.append(dict(project="", task_link="", description="После 15 числа",
                                      hours="", estimated_hours="", parent_task_with_task=""))
         self.output_data.extend(self.second_part_data)
-        hours = sum([log["hours"] for log in self.second_part_data])
+        hours = round(sum([log["hours"] for log in self.second_part_data]), 2)
         self.output_data.append(dict(project="", task_link="", description="", hours=hours, estimated_hours=""))
 
     def get_new_row(self, group_rows):
         new_row = {}
         for group_row in group_rows:
             new_row["project"] = group_row["Project"]
-            new_row["task_link"] = self.task_link + str(group_row["Task Id"])
+            new_row["task_link"] = self.task_link + str(group_row["Task ID"])
             new_row["description"] = group_row["Task"]
-            new_row["parent_task_with_task"] = f"{group_row['Parent Task']} - {group_row['Task']}"
-            new_row["parent_task"] = group_row["Parent Task"]
+            new_row["parent_task_with_task"] = f"{group_row['Parent task']} - {group_row['Task']}"
+            new_row["parent_task"] = group_row["Parent task"]
             if not new_row.get('hours'):
-                new_row["hours"] = float(group_row["Decimal Hours"])
+                new_row["hours"] = float(group_row["Decimal hours"])
             else:
-                new_row["hours"] += float(group_row["Decimal Hours"])
-            new_row["estimated_hours"] = round(float(group_row["Estimated"])/60, 2) if group_row["Estimated"] else ""
+                new_row["hours"] += float(group_row["Decimal hours"])
+            new_row["estimated_hours"] = round(float(group_row["Estimated time"])/60, 2) if group_row["Estimated time"] else ""
         new_row["hours"] = round(new_row["hours"], 2)
         return new_row
         
@@ -117,27 +122,12 @@ class TeamworkExcelParser:
                 date_of_log = group_row["Date"].day
                 if (first_part and date_of_log <= 15) or (not first_part and date_of_log > 15):
                     new_row["project"] = group_row["Project"]
-                    new_row["task_link"] = self.task_link + str(group_row["Task Id"])
+                    new_row["task_link"] = self.task_link + str(group_row["Task ID"])
                     new_row["description"] = group_row["Task"]
                     if not new_row.get('hours'):
                         new_row["hours"] = float(group_row["Decimal Hours"])
                     else:
                         new_row["hours"] += float(group_row["Decimal Hours"])
-                    new_row["estimated_hours"] = round(float(group_row["Estimated"])/60, 2) if group_row["Estimated"] else ""
+                    new_row["estimated_hours"] = round(float(group_row["Estimated time"])/60, 2) if group_row["Estimated time"] else ""
             new_row["hours"] = round(new_row["hours"], 2)
             return new_row if new_row != {} else None
-
-
-
-
-    #     output_data = {}
-    #     for row in data:
-    #         if output_data.get(row["Task Id"]):
-    #             output_data[row['Task Id']] += float(row["Decimal Hours"])
-    #         else:
-    #             output_data[row['Task Id']] = float(row["Decimal Hours"])
-    #         output_data[row["Task Id"]] = self._get_row_data(row, )
-    #     print(output_data)
-    #
-    # def _get_row_data(self, row):
-    #     new_row = {row[''], self.task_link + row['Task Id'], )
